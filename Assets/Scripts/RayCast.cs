07433c03f3dec10cc4b69783ee477b2828f15dee
Mercurius Awad <1015510@student.sae.edu.au>
2022-05-31 16:21:26 +1000

    Basic raycast works
    red raycast on AI collision and green raycast on hitting nothing

---
+++ Assets/Scripts/RayCast.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Physics.Raycast (transform.position, transform.TransformDirection (Vector3.forward), out RaycastHit hitinfo, 20f))
+        {
+            Debug.Log("Hit Something");
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitinfo.distance, Color.red);
+        }
+        else
+        {
+            Debug.Log("Hit Nothing");
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 20f, Color.green);
+
+        }
+    }
+}
